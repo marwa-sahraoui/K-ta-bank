db0fc7c84b1a3ae7ecfd5434fd1b22a8cbcb8b59
marwa sahraoui <marwasahrawi1988@gmail.com>
2022-11-07 13:01:46 +0100

    first test for deposit amount STEP redTest

---
+++ src/main/java/fr/arolla/BankAccount.java
@@ -0,0 +1,20 @@
+package fr.arolla;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+    private String iban;
+    private BigDecimal initBalance;
+
+    public BankAccount(String iban, BigDecimal initBalance) {
+        this.iban = iban;
+        this.initBalance = initBalance;
+    }
+
+    public void deposit(BigDecimal depositAmount) {
+    }
+
+    public BigDecimal getBalance() {
+        return BigDecimal.valueOf(0);
+    }
+}
---
+++ src/test/java/fr/arolla/BankAccountTest.java
@@ -0,0 +1,21 @@
+package fr.arolla;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankAccountTest {
+    @Test
+    public void should_retrun_50_when_depositAmount_50_for_initBalance_with0(){
+    //GIVEN
+        BankAccount bankAccount = new BankAccount("ABC",BigDecimal.valueOf(0));
+    //WHEN
+        bankAccount.deposit(BigDecimal.valueOf(50));
+    //THEN
+        assertThat(bankAccount.getBalance()).isEqualTo(50);
+    }
+
+
+}
