e41ec60a5b7a3e3df54b3dbe6c5370db0b97442c
marwa sahraoui <marwasahrawi1988@gmail.com>
2022-11-07 15:39:22 +0100

    transform transaction class to record

--- src/main/java/fr/arolla/Transaction.java
+++ src/main/java/fr/arolla/Transaction.java
@@ -3,27 +3,6 @@ package fr.arolla;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class Transaction {
+public record Transaction(BigDecimal amount,LocalDate date,TransactionType type) {
 
-    private BigDecimal amount;
-    private LocalDate date;
-    private TransactionType type;
-
-    public Transaction(BigDecimal amount, LocalDate date, TransactionType type) {
-        this.amount = amount;
-        this.date = date;
-        this.type = type;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public TransactionType getType() {
-        return type;
-    }
 }
